fa99743a23cd5c7a2d069be80066f7d4b37279f9
simarjot <simarjot0032@gmail.com>
2023-11-12 01:49:10 +0530

    under cosn page

---
+++ src/components/UnderCons.tsx
@@ -0,0 +1,18 @@
+import Working from "../images/man-working-at-computer-vector-3789594-removebg-preview.png";
+import Clock from "../images/Alarm_Clock_Animation_High_Res.png";
+
+export default function UnderCons() {
+  return (
+    <>
+      {" "}
+      <div className="App">
+        <div className="heading">WEBSITE</div>
+        <div className="secondary">Under Construction</div>
+        <img src={Working} alt="" className="img" />
+        <img src={Clock} alt="" className="imgclock" />
+        <h2 className="heading-launch">Launching Soon!</h2>
+      </div>
+      <div className="footer">Simarjot Singh &copy; 2023</div>
+    </>
+  );
+}
