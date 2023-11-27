d51ccd8086458906ebf181e824cbdf5260c6212b
simarjot <simarjot0032@gmail.com>
2023-11-27 12:06:25 -0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Route, Router, HashRouter } from "react-router-dom";
 import UnderCons from "./components/UnderCons";
 import Navbar from "./components/Navbar";
+import Site from "./components/Site";
 function App() {
   return (
     <>
       <HashRouter>
-        <Navbar></Navbar>
+        <Site />
       </HashRouter>
     </>
   );
--- src/components/Site.tsx
+++ src/components/Site.tsx
@@ -1,9 +1,13 @@
 import "../styles/Site.css";
+import Navbar from "./Navbar";
 
 export default function Site() {
   return (
     <>
-      <div id="Home"></div>
+      <div className="main-container">
+        <Navbar />
+        <div id="Home"></div>
+      </div>
     </>
   );
 }
