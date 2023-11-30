8a3668d029d12db1fe23253ace535fb1b4063272
simarjot <simarjot0032@gmail.com>
2023-11-30 09:04:45 -0700

    update

--- src/components/Site.tsx
+++ src/components/Site.tsx
@@ -1,12 +1,20 @@
 import "../styles/Site.css";
 import Navbar from "./Navbar";
-
+import Image from "../images/1701358933349.webp";
 export default function Site() {
   return (
     <>
       <div className="main-container">
         <Navbar />
-        <div id="Home"></div>
+        <div id="Home">
+          <div className="home-img">
+            <div className="image-div">
+              <img src={Image} alt="" className="image" />
+            </div>
+            <div className="line"></div>
+            <div className="self-name">Simarjot Singh</div>
+          </div>
+        </div>
       </div>
     </>
   );
